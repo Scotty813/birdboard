badafc13f7c2d079b51a52f1a1bf66251ead7a35
Scott Horn <shorn813@outlook.com>
2019-01-25 16:12:26 -0500

    more tests...

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -10,22 +10,51 @@ class ProjectsTest extends TestCase
 {
 
     use WithFaker, RefreshDatabase;
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
+
+    /** @test */
     public function a_user_can_create_a_project()
     {
+
+        $this->withoutExceptionHandling();
 
         $attributes = [
             'title' => $this->faker->sentence,
             'description' => $this->faker->paragraph
         ];
 
-        $this->post('/projects', $attributes);
+        $this->post('/projects', $attributes)->assertRedirect('/projects');
 
         $this->assertDatabaseHas('projects', $attributes);
 
+        $this->get('/projects')->assertSee($attributes['title']);
+
     }
+
+    /** @test */
+    public function a_user_can_view_a_project()
+    {
+        $project = factory('App\Project')->create();
+
+        $this->get('/projects/' . $project->id)
+            ->assertSee($project->title)
+            ->assertSee($project->description);
+    }
+
+
+    /** @test */
+    public function a_project_requires_a_title()
+    {
+        $attributes = factory('App\Project')->raw(['title' => '']);
+
+        $this->post('/projects', $attributes)->assertSessionHasErrors('title');
+    }
+
+
+    public function a_project_requires_a_description()
+    {
+        $attributes = factory('App\Project')->raw(['description' => '']);
+
+        $this->post('/projects', $attributes)->assertSessionHasErrors('description');
+    }
+
 }
